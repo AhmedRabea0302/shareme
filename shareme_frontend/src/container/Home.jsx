71afd6e360fcd5ffdd18d528f8c1f152cd051265
Fadloovich <arabea169@gmail.com>
2023-02-18 21:43:33 +0200

    Added Sidebar Component 🔥

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -25,44 +25,44 @@ const Home = () => {
   }, []);
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen duration-75 ease-out">
-      <div
-        className="hidden md:flex h-scree
-       flex-initial"
-      >
+      <div className="hidden md:flex h-scree flex-initial">
         <Sidebar user={user && user} />
       </div>
 
       <div className="flex md:hidden flex-row">
-        <HiMenu
-          fontSize={40}
-          className="cursor-pointer"
-          onClick={() => setToggleSidebar(true)}
-        />
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28" />
-        </Link>
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
 
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="logo" className="w-28" />
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-id">
-          <div
-            className="absolute w-full flex
-           justify-end items-center p-2"
-          >
-            <AiFillCloseCircle
-              fontSize={30}
-              className="cursor-pointer"
-              onClick={() => {
-                setToggleSidebar(false);
-              }}
-            />
-            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
-          </div>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="logo" className="w-28" />
+          </Link>
         </div>
-      )}
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-id">
+            <div
+              className="absolute w-full flex
+           justify-end items-center p-2"
+            >
+              <AiFillCloseCircle
+                fontSize={30}
+                className="cursor-pointer"
+                onClick={() => {
+                  setToggleSidebar(false);
+                }}
+              />
+              <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+            </div>
+          </div>
+        )}
+      </div>
+
       <div
         className="pb-2 flex-1 h-screen overflow-y-sc
       "
